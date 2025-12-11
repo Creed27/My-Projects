a74f1f5c282ee1b308e7c801db52851ee17f9caf
Andrea Cavallo <152169214+Creed27@users.noreply.github.com>
2025-12-11 11:30:52 +0100

    Update main.cu

--- Thesis/RRSplit_GPU/main.cu
+++ Thesis/RRSplit_GPU/main.cu
@@ -1,21 +1,3 @@
-/*
- *  graphISO: Tools to compute the Maximum Common Subgraph between two graphs
- *  Copyright (c) 2019 Stefano Quer
- *  
- *  This program is free software : you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.If not, see < http: *www.gnu.org/licenses/>
- */
-
 #include <algorithm>
 #include <functional>
 #include <numeric>
@@ -1026,4 +1008,5 @@ int main(int argc, char** argv) {
 	//cout << arguments.filename1 << "," << arguments.filename2 << "," << (int)sol_len << "," << time_elapsed << endl;
 	writer.close();
     return 0;
+
 }
